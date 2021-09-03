74272b12a7bed17e80584627c88d752fa31784d5
Teja <tejaswigajula@gmail.com>
2021-09-03 15:16:49 +0530

    maths calculations added

--- samplescript.sh
+++ samplescript.sh
@@ -4,3 +4,8 @@ x=10
 y=20
 echo "sum of $x and $y = $((x+y))"
 echo "multipliction of $x and $y = $((x*y))"
+if [[$x gt $y]]
+then
+     echo "subtraction of $x and $y = $((x-y))"
+else
+    echo "subtraction of $y and $x = $((y-x))"     
